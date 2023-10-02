62c13f90326b5295a038e262b9cdd1c675a8db46
brunogabrielti <brunogabrielti@hotmail.com>
2023-10-02 00:08:05 -0300

    v2.0

---
+++ js/js.js
@@ -0,0 +1,34 @@
+   
+ $("li").click(function() {
+
+
+    if (this.id == "apresentacao-btn"){
+        $("#apresentacao").css("display","flex");
+        $("#projetos").css("display","none");
+        $("#contato").css("display","none");
+        $(this).addClass("botao-ativo");
+        $("#projetos-btn").removeClass("botao-ativo");
+        $("#contato-btn").removeClass("botao-ativo");
+    }  
+
+    if (this.id == "projetos-btn"){
+        $("#projetos").css("display","flex");
+        $("#apresentacao").css("display","none");
+        $("#contato").css("display","none");
+        $(this).addClass("botao-ativo");
+        $("#apresentacao-btn").removeClass("botao-ativo");
+        $("#contato-btn").removeClass("botao-ativo");
+    } 
+
+    if (this.id == "contato-btn"){
+        $("#contato").css("display","flex");
+        $("#apresentacao").css("display","none");
+        $("#projetos").css("display","none");
+
+        $(this).addClass("botao-ativo");
+        $("#apresentacao-btn").removeClass("botao-ativo");
+        $("#projetos-btn").removeClass("botao-ativo");
+        
+    } 
+
+});
