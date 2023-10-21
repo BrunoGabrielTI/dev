75da7702baec13bcbf0a684fdfa2de3a4347d2ae
Bruno Gabriel <95447487+BrunoGabrielTI@users.noreply.github.com>
2023-10-21 14:28:35 -0300

    v2.2.1

--- js/js.js
+++ js/js.js
@@ -1,38 +1,43 @@
+/* Bruno Gabriel Developer - 2023 */   
+/* Código simples apenas para garantir as funcionalidades propostas pela página estática.*/   
+
+$(document).ready(function(){ /* Garante o carregamento da página. Colocado na versão 2.2.1 */
    
- $("li").click(function() {
-
-
-    if (this.id == "apresentacao-btn"){
+   document.body.style.zoom = 1.0; /* Garante que o zoom da página esteja em 1x. Colocado na versão 2.2.1 */
+   
+   $("li").click(function() { /* Código da navegação. */
+   
+   
+      if (this.id == "apresentacao-btn"){
         $("#apresentacao").css("display","flex");
         $("#projetos").css("display","none");
         $("#contato").css("display","none");
         $(this).addClass("botao-ativo");
         $("#projetos-btn").removeClass("botao-ativo");
         $("#contato-btn").removeClass("botao-ativo");
-    }  
-
-    if (this.id == "projetos-btn"){
+      }  
+      
+      if (this.id == "projetos-btn"){
         $("#projetos").css("display","flex");
         $("#apresentacao").css("display","none");
         $("#contato").css("display","none");
         $(this).addClass("botao-ativo");
         $("#apresentacao-btn").removeClass("botao-ativo");
         $("#contato-btn").removeClass("botao-ativo");
-    } 
-
-    if (this.id == "contato-btn"){
+      } 
+      
+      if (this.id == "contato-btn"){
         $("#contato").css("display","flex");
         $("#apresentacao").css("display","none");
         $("#projetos").css("display","none");
-
+      
         $(this).addClass("botao-ativo");
         $("#apresentacao-btn").removeClass("botao-ativo");
         $("#projetos-btn").removeClass("botao-ativo");
         
-    } 
+      } 
+      
+   });
 
-});
-
-$(document).ready(function(){
-  document.body.style.zoom = 1.0
+  
 });
